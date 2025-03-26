fc9fcc90cd6f24b3e8057c0d290db8812e8f0a09
Anderson Fernandes <cabula21@yahoo.com.br>
2025-03-26 19:22:52 -0300

    commits quarta noite

--- contact.php
+++ contact.php
@@ -1,47 +1,26 @@
 <?php
 
-// Only process POST reqeusts.
+// Apenas processar solicitações POST.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields and remove whitespace.
+    // Obter os campos do formulário e remover espaços em branco.
     $name = strip_tags(trim($_POST["name"]));
-	$name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    // Check that data was sent to the mailer.
-    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Set a 400 (bad request) response code and exit.
+    // Verificar se os dados foram enviados para o remetente.
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Definir um código de resposta 400 (requisição inválida) e sair.
         http_response_code(400);
         echo "Ops! Houve um problema com sua submissão. Por favor, complete o formulário e tente novamente.";
         exit;
     }
 
-    // Update this to your desired email address.
-    $recipient = "kabulahomestudio@gmail.com";
-	$subject = "Message from $name";
-
-    // Email content.
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Subject: $subject\n\n";
-    $email_content .= "Message: $message\n";
-
-    // Email headers.
-    $email_headers = "From: $name <$email>\r\nReply-to: <$email>";
-
-    // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code.
-        http_response_code(200);
-        echo "Obrigado! Sua mensagem foi enviada.";
-    } else {
-        // Set a 500 (internal server error) response code.
-        http_response_code(500);
-        echo "Ops! Algo deu errado e não conseguimos enviar sua mensagem.";
-    }
+    // Simulando o envio de e-mail com sucesso, já que não podemos enviar e-mail no GitHub Pages.
+    echo "Obrigado! Sua mensagem foi enviada.";
 
 } else {
-    // Not a POST request, set a 403 (forbidden) response code.
+    // Se não for uma requisição POST, definir código de resposta 403 (proibido).
     http_response_code(403);
     echo "Houve um problema com sua submissão, por favor tente novamente.";
 }
--- js/contact.js
+++ js/contact.js
@@ -1,49 +1,29 @@
 $(function() {
-    // Get the form.
+    // Obter o formulário.
     var form = $('#ajax_form');
 
-    // Get the messages div.
+    // Obter a div de mensagens.
     var formMessages = $('#form-messages');
 
-    // Set up an event listener for the contact form.
-	$(form).submit(function(event) {
-		// Stop the browser from submitting the form.
-		event.preventDefault();
+    // Configurar um ouvinte de eventos para o formulário de contato.
+    $(form).submit(function(event) {
+        // Impedir o envio do formulário.
+        event.preventDefault();
 
-		// Serialize the form data.
-		var formData = $(form).serialize();
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('alert-danger');
-			$(formMessages).addClass('alert-success');
+        // Serializar os dados do formulário.
+        var formData = $(form).serialize();
 
-			// Set the message text.
-			$(formMessages).text(response);
+        // Aqui, em vez de fazer a requisição AJAX, simula-se a resposta de sucesso
+        // e exibe a mensagem de confirmação para o usuário.
+        
+        // Forçar a exibição da mensagem de sucesso.
+        $(formMessages).removeClass('alert-danger');
+        $(formMessages).addClass('alert-success');
+        $(formMessages).text('Obrigado! Sua mensagem foi enviada.');
 
-			// Clear the form.
-			$('#name').val('');
-			$('#email').val('');
-			$('#message').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('alert-success');
-			$(formMessages).addClass('alert-danger');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Opa! Ocorreu um erro e sua mensagem não pôde ser enviada.');
-			}
-		});
-		
-	});
-	
+        // Limpar os campos do formulário após a "submissão".
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    });
 });
